bed4a627e22dac7c68690ab7dc5097251d1f40bd
Aryan <107269876+pikachu71213@users.noreply.github.com>
2026-06-04 10:57:35 +0530

    update Leadership component to reflect full name of Mr. Aryan Saini

--- webfrontend/src/modules/website/pages/Home/Leadership.jsx
+++ webfrontend/src/modules/website/pages/Home/Leadership.jsx
@@ -85,7 +85,7 @@ const Leadership = () => {
                   fontSize: "clamp(28px, 5vw, 52px)",
                 }}
               >
-                Mr. Aryan
+                Mr. Aryan Saini
               </h2>
 
               {/* Orange underline */}
